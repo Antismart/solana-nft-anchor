536e9474c97f0fc4f4c9fbd83e70626614532a0f
Moses Timbwa <timbwamoses83@gmail.com>
2024-06-29 15:29:58 +0300

    added a space to serve as a visual separation between the accounts and Programs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -22,7 +22,7 @@ pub mod solana_nft_anchor {
 pub struct InitNFT<'info> {
     /// CHECK: ok, we are passing in this account ourselves
     #[account(mut, signer)]
-    signer: AccountInfo<'info>,
+    pub signer: AccountInfo<'info>,
     #[account(
         init,
         payer = signer,
@@ -31,4 +31,16 @@ pub struct InitNFT<'info> {
         mint::freeze_authority = signer.key(),
     )]
     pub mint: Account<'info, Mint>,
+    #[acccount(
+        init_if_ended,
+        payer = signer,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+    )]
+    pub associated_token_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>    
 }
